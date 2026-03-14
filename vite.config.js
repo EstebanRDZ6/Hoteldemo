b0b43ff84effdead83628f7cc04a9d04686d928e
Esteban <estebanrdrgz6@gmail.com>
2026-03-14 17:31:03 -0300

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/Hoteldemo/' : '/',
   plugins: [react()],
   server: {
     host: true,
     port: 5173,
   },
-})
+}))
